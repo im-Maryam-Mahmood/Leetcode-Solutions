50337fb8253e62fc7174314e1a64b497076e3716
Maryam Mahmood <159541999+im-Maryam-Mahmood@users.noreply.github.com>
2024-06-27 03:19:57 +0500

    Time: 5 ms (28.14%) | Memory: 11.1 MB (32.67%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -8,12 +8,13 @@ public:
        while( p1 >=0 && p2 >=0 ){
         if( nums1[p1] > nums2[p2]){
             nums1[p]  = nums1[p1];
+            p--;
         p1--;
         }else{
             nums1[p] = nums2[p2];
-        p2--;}
+        p2--;
         p--;
-       }
+       }}
        while( p2 >=0 ){
         nums1[p] = nums2[p2];
         p2--;

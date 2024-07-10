61641bb4c4e07e5e4ad9633c7af84f01db5be0e8
Maryam Mahmood <159541999+im-Maryam-Mahmood@users.noreply.github.com>
2024-07-10 05:00:01 +0500

    Time: 150 ms (59.48%) | Memory: 76.7 MB (37.03%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.cpp
+++ 1803-average-waiting-time/average-waiting-time.cpp
@@ -7,20 +7,12 @@ public:
         for (const auto& customer : customers) {
             int arrivalTime = customer[0];
             int cookingTime = customer[1];
-
-            // Update current time to when the chef starts this customer's order
             if (currentTime < arrivalTime) {
                 currentTime = arrivalTime;
             }
-            
-            // Calculate the finish time for this order
             currentTime += cookingTime;
-            
-            // Calculate the waiting time for this customer
             totalWaitingTime += (currentTime - arrivalTime);
         }
-
-        // Calculate and return the average waiting time
         return (double)totalWaitingTime / customers.size();
     }
 };

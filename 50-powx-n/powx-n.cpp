00422ff3575dc2d4d1455ed9c936ae9c35d4171f
Maryam Mahmood <159541999+im-Maryam-Mahmood@users.noreply.github.com>
2024-07-12 19:20:03 +0500

    Time: 0 ms (100.00%) | Memory: 8.3 MB (10.78%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        double ans;
-        ans = pow( x, n );
-        return ans;
+        if (n == 0) return 1.0;
+        if (n == 1) return x;
+        if (n == -1) return 1 / x;
+        double i = myPow(x, n / 2);
+        double j = myPow(x, n % 2);
+        return i * i * j;
     }
 };

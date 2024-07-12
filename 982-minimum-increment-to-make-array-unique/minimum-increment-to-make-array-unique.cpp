6150f8664272bbc413fca484fdc2101fac401f75
Maryam Mahmood <159541999+im-Maryam-Mahmood@users.noreply.github.com>
2024-07-13 01:46:28 +0500

    Time: 122 ms (47.28%) | Memory: 69.3 MB (25.45%) - LeetSync

---
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& nums) {
+        if (nums.empty()) return 0;
+        int moves = 0;
+        sort(nums.begin(), nums.end());
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] <= nums[i - 1]) {
+                int increment = nums[i - 1] - nums[i] + 1;
+                nums[i] += increment;
+                moves += increment;
+            }
+        }
+        return moves;
+    }
+};

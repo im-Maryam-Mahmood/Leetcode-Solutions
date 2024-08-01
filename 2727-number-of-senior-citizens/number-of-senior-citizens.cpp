65e1d5563ae0be44a013738eb9c9a578c2fdb0ce
Maryam Mahmood <159541999+im-Maryam-Mahmood@users.noreply.github.com>
2024-08-01 23:24:47 +0500

    Time: 8 ms (47.01%) | Memory: 17.5 MB (31.37%) - LeetSync

---
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int count = 0;
+        for(const auto& detail : details) {
+            int age = (detail[11] - '0') * 10 + (detail[12] - '0');
+            if (age > 60) count++;
+        }
+        return count;
+    }
+};

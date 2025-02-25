48ad4b2010dc046d483835e9d9a9acc8371d881c
Maryam Mahmood <159541999+im-Maryam-Mahmood@users.noreply.github.com>
2025-02-26 00:03:46 +0500

    Time: 11 ms (51.70%) | Memory: 32.1 MB (18.94%) - LeetSync

--- 561-array-partition/array-partition.cpp
+++ 561-array-partition/array-partition.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     int arrayPairSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int maxSum = 0;
-        for (int i = 0; i < nums.size(); i += 2) maxSum += nums[i];
-        return maxSum;
+    sort(nums.begin(), nums.end());
+    int sum = 0;
+    for(int i =0; i<nums.size(); i+=2){
+        sum += nums[i];
+    }
+    return sum;
     }
 };
